7056edc4a661fb081f0c9853c149081e541183d5
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2024-09-02 10:21:26 +0800

    add wc policy

--- main.go
+++ main.go
@@ -11,10 +11,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 递归读取目录并注册路由
+func registerHTMLRoutes(router *gin.Engine, baseDir string, prefix string) error {
+	files, err := os.ReadDir(baseDir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		path := filepath.Join(baseDir, file.Name())
+		if file.IsDir() {
+			// 如果是目录，递归调用
+			err := registerHTMLRoutes(router, path, prefix+file.Name()+"/")
+			if err != nil {
+				return err
+			}
+		} else if strings.HasSuffix(strings.ToLower(file.Name()), ".html") {
+			// 如果是 HTML 文件，注册路由
+			urlPath := prefix + strings.TrimSuffix(file.Name(), ".html")
+			router.GET(urlPath, func(c *gin.Context) {
+				c.File(path)
+			})
+		}
+	}
+	return nil
+}
+
 func main() {
 	r := gin.Default()
 	//router.StaticFS("/", http.Dir(filepath.Join(dir, "./html")))
 
+	// 注册默认路由
 	r.GET("/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, TLS user! Your config: %s", c.Request.TLS)
 	})
@@ -25,23 +52,10 @@ func main() {
 
 	r.Static("/static", "./static")
 
-	// 遍历html目录的一级深度
-	files, err := os.ReadDir("./html")
+	// 递归注册 HTML 路由
+	err := registerHTMLRoutes(r, "./html", "/")
 	if err != nil {
-		log.Fatalf("Failed to read directory: %v", err)
-	}
-
-	for _, file := range files {
-		if !file.IsDir() && strings.HasSuffix(strings.ToLower(file.Name()), ".html") {
-			// 构建URL路径，去除".html"扩展名
-			urlPath := "/" + strings.TrimSuffix(file.Name(), ".html")
-
-			// 注册路由
-			r.GET(urlPath, func(c *gin.Context) {
-				// 提供HTML文件
-				c.File(filepath.Join("./html", file.Name()))
-			})
-		}
+		log.Fatalf("Failed to register HTML routes: %v", err)
 	}
 
 	go func() {
