c8d0d9ced2d1d619b107cf566cc583e5175cdfd7
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2024-09-05 11:21:20 +0800

    update

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 递归读取目录并注册路由
 func registerHTMLRoutes(router *gin.Engine, baseDir string, prefix string) error {
 	files, err := os.ReadDir(baseDir)
 	if err != nil {
@@ -22,13 +21,11 @@ func registerHTMLRoutes(router *gin.Engine, baseDir string, prefix string) error
 	for _, file := range files {
 		path := filepath.Join(baseDir, file.Name())
 		if file.IsDir() {
-			// 如果是目录，递归调用
 			err := registerHTMLRoutes(router, path, prefix+file.Name()+"/")
 			if err != nil {
 				return err
 			}
 		} else if strings.HasSuffix(strings.ToLower(file.Name()), ".html") {
-			// 如果是 HTML 文件，注册路由
 			urlPath := prefix + strings.TrimSuffix(file.Name(), ".html")
 			router.GET(urlPath, func(c *gin.Context) {
 				c.File(path)
@@ -42,7 +39,6 @@ func main() {
 	r := gin.Default()
 	//router.StaticFS("/", http.Dir(filepath.Join(dir, "./html")))
 
-	// 注册默认路由
 	r.GET("/cookie", func(c *gin.Context) {
 		var domain = c.Query("domain")
 		var key = c.Query("key")
@@ -72,7 +68,6 @@ func main() {
 
 	r.Static("/static", "./static")
 
-	// 递归注册 HTML 路由
 	err := registerHTMLRoutes(r, "./html", "/")
 	if err != nil {
 		log.Fatalf("Failed to register HTML routes: %v", err)
